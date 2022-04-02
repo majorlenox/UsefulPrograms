83eac7ca083a9356a5215086c43f9267dd31d3fb
majorlenox <majorlenox@mail.ru>
2022-04-02 14:06:12 +0300

    added n = p^2*q^2

--- RSA_p_q/src/Main.java
+++ RSA_p_q/src/Main.java
@@ -2,14 +2,15 @@ import java.math.BigInteger;
 
 public class Main {
 
-    public static void main(String[] args){
+    private static boolean option = true;  // false -> n = p*q, true -> n = p^2*q^2
 
-        BigInteger n = new BigInteger("1584586296183412107468474423529992275940096154074798537916936609523894209759157543");
+    public static void main(String[] args){
+        BigInteger n = new BigInteger("184906977989893071784194758558769080224895044140931441118833206249383803179734573611321");
         BigInteger e = new BigInteger("65537");
-        BigInteger c = new BigInteger("964354128913912393938480857590969826308054462950561875638492039363373779803642185");
+        BigInteger c = new BigInteger("175698528051807837145146484599295985521262017741071537613661229632145002128053611973544");
         // find p, q from factordb.com
-        BigInteger p = new BigInteger("2434792384523484381583634042478415057961");
-        BigInteger q = new BigInteger("650809615742055581459820253356987396346063");
+        BigInteger p = new BigInteger("3661161386942196028943");
+        BigInteger q = new BigInteger("3714135784936599300827");
         BigInteger f = Find_f(p, q);
         BigInteger d = Find_d(e,f);
         BigInteger m = Decrypt(c,d,n);
@@ -17,11 +18,16 @@ public class Main {
     }
 
     static BigInteger Find_f(BigInteger p, BigInteger q){
-        return p.add(new BigInteger("-1")).multiply(q.add(new BigInteger("-1")));
+        BigInteger f = p.add(new BigInteger("-1")).multiply(q.add(new BigInteger("-1")));
+        if (option) {
+            return p.multiply(q).multiply(f);
+        }else {
+            return f;
+        }
     }
 
     static BigInteger Find_d(BigInteger e, BigInteger f){
-         return e.modInverse(f);
+        return e.modInverse(f);
     }
 
     static BigInteger Decrypt(BigInteger c, BigInteger d, BigInteger n){
