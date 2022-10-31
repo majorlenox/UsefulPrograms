db68c2aa8f73c426aea17ab2ce26f4cd7dc4ef0d
majorlenox <majorlenox@mail.ru>
2022-10-31 13:22:53 +0300

    now got norm distrib and 2 graphs on one plot

--- Visualize_function/main.py
+++ Visualize_function/main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import norm
+
 
 def F0(x):
     return ((lr(x, 1)) ^ (lr(x, 2)) ^ (lr(x, 7))) % 256
@@ -53,6 +55,12 @@ if __name__ == '__main__':
     df_F1 = F1toDataFrame()
     distribution_of_hw_F0 = [0] * 9
     distribution_of_hw_F1 = [0] * 9
+    distribution_of_hw_x = [0] * 9
+    arguments = [0]*256
+    for i in range(256):
+        arguments[i] = i
+    for x in df_F0['x hw']:
+        distribution_of_hw_x[x] += 1
     for F0_hw in df_F0['F0 hw']:
         distribution_of_hw_F0[F0_hw] += 1
     for F1_hw in df_F1['F1 hw']:
@@ -60,11 +68,34 @@ if __name__ == '__main__':
     print(distribution_of_hw_F0)
     print(distribution_of_hw_F1)
     pd.set_option("display.max.columns", None)
+
+    plt.plot(arguments, df_F0['F0 hw'], label="F0", color='tab:blue')
+    plt.plot(arguments, df_F1['F1 hw'], label="F1", color='tab:orange')
+    plt.title('F0 and F1 hamming weight distribution')
+    plt.ylabel('hamming weight')
+    plt.xlabel('argument')
+    plt.legend()
+    plt.show()
+
+    n_bins = 15
+    plt.hist([df_F0['F0 hw'], df_F1['F1 hw']], n_bins, density=True, color=['tab:blue', 'tab:orange'], label=["F0", "F1"], histtype='bar')
+    plt.title('F0 and F1 hamming weight distribution')
+    plt.xlabel('hamming weight')
+    mu = 4
+    sigma = 0.8
+    x = np.arange(0, 8, 0.001)
+    plt.plot(x, norm.pdf(x, mu, sigma), '--', color='black')
+    plt.legend(prop={'size': 12})
+    plt.show()
+
+"""
     df_F0.plot(x='x(2)', y='F0 hw', kind='area')
     plt.show()
     df_F1.plot(x='x(2)', y='F1 hw', kind='area')
     plt.show()
-
-    plt.plot(distribution_of_hw_F0)
-    #add_titlebox(ax2, 'Histogram: home age')
+    plt.plot(distribution_of_hw_x)
+    plt.title('Hamming weight x distribution')
     plt.show()
+    plt.plot(distribution_of_hw_F0)
+    plt.show()
+"""
